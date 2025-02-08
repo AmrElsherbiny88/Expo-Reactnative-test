57cc626a40a6e74a50dd709f31f2fece055b782c
AmrElsherbiny <amoryelsherbiny@gmail.com>
2025-02-08 06:36:17 +0200

    Update message.tsx

--- app/(home)/Components/message.tsx
+++ app/(home)/Components/message.tsx
@@ -10,10 +10,10 @@ interface MessageProps {
 
 const Message: React.FC<MessageProps> = ({ content, time, username }) => {
   return (
-    <View style={tw`bg-gray-200 p-3 rounded-lg shadow-md my-2 max-w-[75%] self-end`}>
-      <Text style={tw`text-blue-600 font-semibold`}>{username}</Text>
-      <Text style={tw`text-black break-words`}>{content}</Text>
-      <Text style={tw`text-xs text-gray-500 text-right mt-2`}>{time}</Text>
+    <View style={tw`bg-green-800 p-3 rounded-tr-xl rounded-tl-xl rounded-bl-xl   my-2 max-w-[75%] self-end`}>
+      {/* <Text style={tw`text-blue-600 font-semibold`}>{username}</Text> */}
+      <Text style={tw`text-white break-words`}>{content}</Text>
+      <Text style={tw`text-xs text-green-100 text-left  mt-1`}>{time}</Text>
     </View>
   );
 };
