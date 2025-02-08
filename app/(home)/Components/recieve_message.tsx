1cd205678789bb7d80ccf99ba5fd054752f9cd69
AmrElsherbiny <amoryelsherbiny@gmail.com>
2025-02-08 06:36:23 +0200

    Update recieve_message.tsx

--- app/(home)/Components/recieve_message.tsx
+++ app/(home)/Components/recieve_message.tsx
@@ -10,11 +10,16 @@ interface ReceiveMessageProps {
 
 const ReceiveMessage: React.FC<ReceiveMessageProps> = ({ content, time, username }) => {
   return (
-    <View style={tw`bg-gray-200 p-3 rounded-lg shadow-md my-2 max-w-[80%] self-start`}>
-      <Text style={tw`text-green-700 font-semibold`}>{username}</Text>
-      <Text style={tw`text-black break-words`}>{content}</Text>
-      <Text style={tw`text-xs text-gray-500 text-right mt-2`}>{time}</Text>
-    </View>
+<View>
+      <View>
+         <Text style={tw`text-green-700 font-semibold`}>{username}</Text>
+      </View>
+      <View style={tw`bg-white p-3 rounded-tr-xl rounded-tl-xl rounded-br-xl shadow-md my-2 max-w-[80%] self-start`}>
+       
+        <Text style={tw`text-green-900 break-words`}>{content}</Text>
+        <Text style={tw`text-xs text-green-700 text-left mt-1`}>{time}</Text>
+      </View>
+</View>
   );
 };
 
