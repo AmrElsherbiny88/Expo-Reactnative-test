15948de1b63a44a49be6b2e2364eff678bfcbc6a
AmrElsherbiny <amoryelsherbiny@gmail.com>
2025-02-08 06:36:10 +0200

    Update _layout.tsx

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,13 +1,14 @@
 import { Stack } from 'expo-router'
 import React from 'react'
+import Header from './Components/Header'
 
 const _layout = () => {
   return (
     <Stack>
         <Stack.Screen  name="index" options={{headerStyle:{
-          backgroundColor: 'black'
+          backgroundColor: '#FAF3E9'
         } , 
-        headerTitle:"Chat App" 
+        headerTitle:() => <Header /> 
         
         }} />
         <Stack.Screen  name="test"  />
